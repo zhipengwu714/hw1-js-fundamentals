9746e629b3bb8d6a7d2997ec473559397c61f214
ZhiPengWu <zhipeng.wu44@bcmail.cuny.edu>
2026-02-02 00:45:04 -0500

    Added Min and Max functions

--- hw1.js
+++ hw1.js
@@ -1,4 +1,4 @@
-const data = [1,2,3];
+const data1 = [1,2,3];
 
 function sum(arr) {
     let total = 0;
@@ -8,11 +8,32 @@ function sum(arr) {
     return total;
 }
 
-console.log("Sum: ", sum(data))
+console.log("Sum: ", sum(data1))
 
 function average(arr) {
-    return sum(data)/data.length;
+    return sum(arr)/arr.length;
 }
 
-console.log("Average: ", average(data))
+console.log("Average: ", average(data1))
 
+const data2 = [3,1,2];
+
+function min(arr) {
+    let min = arr[0];
+    for(const n of arr) {
+        if(min>n) {min=n;}
+    }
+    return min;
+}
+
+console.log("Min: ", min(data2))
+
+function max(arr) {
+    let max = arr[0];
+    for (const n of arr) {
+        if (max<n) {max=n;}
+    }
+    return max;
+}
+
+console.log("Max: ", max(data2))
